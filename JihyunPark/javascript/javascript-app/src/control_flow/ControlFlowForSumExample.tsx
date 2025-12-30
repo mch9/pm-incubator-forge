059843cd97de1eee73d524364723c0680017ad1e
Jihyun Park <90372829+jihyun-p@users.noreply.github.com>
2025-12-30 16:46:51 +0900

    Update ControlFlowForSumExample.tsx

--- JihyunPark/javascript/javascript-app/src/control_flow/ControlFlowForSumExample.tsx
+++ JihyunPark/javascript/javascript-app/src/control_flow/ControlFlowForSumExample.tsx
@@ -4,10 +4,23 @@ export const ControlFlowForSumExample = () => {
 
     // j 기준에서는 2
     // i 기준에서는 5로 지정하면 됨
-    for (let i = 1, j = 0; i <= 2; i += 2, j++) {
+    for (let i = 1, j = 0; j <= 2; i += 2, j++) {
         loopResultArray.push(i)
         summation += loopResultArray[j]
     }
+
+    // 결과는 전부 동일 
+    // j 기준 (동의어 1)
+    // for (let i = 1, j = 1; i <= 3; i += 2, j++) {
+    //     loopResultArray.push(i)
+    //     summation += loopResultArray[j-1]
+    // }
+
+    // i 기준 (동의어 2)
+    // for (let i = 1, j = 1; i <= 5; i += 2, j++) {
+    //     loopResultArray.push(i)
+    //     summation += loopResultArray[j-1]
+    // }
 
     return (
         <div>
