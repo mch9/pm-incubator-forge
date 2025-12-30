6c59c8e7aee7729621b37c6be82dc751936cfe7b
rosyroad03-sys <rosyroad03@gmail.com>
2025-12-30 15:28:52 +0900

    feat: [신희도] for 문 활용하여 배열값 합산하기 [PMIF-x]

--- SHINHUIDO/javascript/javascript-app/src/App.tsx
+++ SHINHUIDO/javascript/javascript-app/src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css'
+import { ControlFlowFor } from './control_flow/ControlFlowFor'
+import { ControlFlowForSummation } from './control_flow/ControlFlowForSummation'
 import { ControlFlowIf } from './control_flow/ControlFlowIf'
 import { ControlFlowIfSwitch } from './control_flow/ControlFlowSwitch'
 import { VariableAndDataTypes } from './data_type/VariableAndDataTypes'
@@ -17,6 +19,8 @@ function App() {
         <ControlFlowIf/>
         <FirstProblem/>
         <ControlFlowIfSwitch/>
+        <ControlFlowFor/>
+        <ControlFlowForSummation/>
       </div>
     </>
   )
